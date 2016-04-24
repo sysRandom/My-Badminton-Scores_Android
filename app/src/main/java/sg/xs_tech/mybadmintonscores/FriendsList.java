06fb0c7b4298aa15a3ac8976dc9740f0482c90c1
Jason <jason@xs-tech.sg>
2016-04-24 23:28:43 +0800

    Set up ListView onClick Listener

--- app/src/main/java/sg/xs_tech/mybadmintonscores/FriendsList.java
+++ app/src/main/java/sg/xs_tech/mybadmintonscores/FriendsList.java
@@ -1,9 +1,15 @@
 package sg.xs_tech.mybadmintonscores;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class FriendsList extends AppCompatActivity {
 
@@ -16,6 +22,16 @@ public class FriendsList extends AppCompatActivity {
         mFriendList = (ListView) findViewById(R.id.friends_list);
 
         Intent intent = getIntent();
-        String jsonFriends = intent.getStringExtra("fbFriends");
+        ArrayList<Friend> friends = intent.getParcelableArrayListExtra("fbFriends");
+
+        FriendAdapter adapter = new FriendAdapter(this, friends);
+        mFriendList.setAdapter(adapter);
+        mFriendList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Friend selected = (Friend) parent.getItemAtPosition(position);
+                Log.i(this.toString(), String.format(Locale.getDefault(), "ID: %s, Name: %s", selected.getId(), selected.getFname()));
+            }
+        });
     }
 }
