4c74c7e93f13484bc6a89038c69db8fc4667596a
Jason <jason@xs-tech.sg>
2016-05-03 00:51:20 +0800

    Fixed bug in Async display of profile pictures

--- app/src/main/java/sg/xs_tech/mybadmintonscores/Friend.java
+++ app/src/main/java/sg/xs_tech/mybadmintonscores/Friend.java
@@ -1,43 +1,52 @@
 package sg.xs_tech.mybadmintonscores;
 
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 public class Friend implements Parcelable {
-    private String id;
+    private String id = "";
     private String fname = "";
     private String email = "";
     private String profile_picture_url = "";
-    private Bitmap profile_picture;
+    private Bitmap profile_picture = null;
 
     public Friend(String fname) {
         this.fname = fname;
     }
-
     public Friend(String id, String fname) {
         this.id = id;
         this.fname = fname;
+//        retrieveProfileBitmapURL();
     }
-
     protected Friend(Parcel in) {
         id = in.readString();
         fname = in.readString();
         email = in.readString();
     }
-
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(id);
         dest.writeString(fname);
         dest.writeString(email);
     }
-
     @Override
     public int describeContents() {
         return 0;
     }
-
     public static final Creator<Friend> CREATOR = new Creator<Friend>() {
         @Override
         public Friend createFromParcel(Parcel in) {
@@ -49,42 +58,64 @@ public class Friend implements Parcelable {
             return new Friend[size];
         }
     };
-
     protected String getId() {
         return id;
     }
-
     public void setId(String id) {
         this.id = id;
     }
-
     public String getFname() {
         return fname;
     }
-
     public void setFname(String fname) {
         this.fname = fname;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getProfile_picture_url() {
         return profile_picture_url;
     }
-
-    public void setProfile_picture_url(String profile_picture_url) {
-        this.profile_picture_url = profile_picture_url;
+    public void getProfile_picture(ImageView imageView) {
+        retrieveProfileBitmapURL(imageView);
     }
-    public Bitmap getProfile_picture() {
-        return profile_picture;
+    private void downloadProfileBitmap(ImageView imageView) throws ExecutionException, InterruptedException {
+        if (profile_picture_url.isEmpty()) return;
+        if (profile_picture != null) return;
+        Log.i(this.toString(), "Downloading Profile picture Bitmap");
+        profile_picture = new DownloadProfilePictureTask().execute(profile_picture_url).get();
+        imageView.setImageBitmap(profile_picture);
     }
-    public void setProfile_picture(Bitmap profile_picture) {
-        this.profile_picture = profile_picture;
+    private void retrieveProfileBitmapURL(final ImageView imageView) {
+        if (id.isEmpty()) return;
+        Bundle param = new Bundle();
+        param.putBoolean("redirect", false);
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/" + id + "/picture",
+                param, HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        try {
+                            JSONObject mData = response.getJSONObject().getJSONObject("data");
+                            if (mData.has("url")) {
+                                try {
+                                    Log.i(this.toString(), "Profile URL for " + fname + ": " + mData.getString("url"));
+                                    profile_picture_url = mData.getString("url");
+                                    downloadProfileBitmap(imageView);
+                                } catch (ExecutionException|InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
     }
 }
--- app/src/main/java/sg/xs_tech/mybadmintonscores/FriendAdapter.java
+++ app/src/main/java/sg/xs_tech/mybadmintonscores/FriendAdapter.java
@@ -1,9 +1,6 @@
 package sg.xs_tech.mybadmintonscores;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,16 +8,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
 
 public class FriendAdapter extends ArrayAdapter<Friend> {
     private ImageView ivProfilePic;
@@ -32,54 +20,15 @@ public class FriendAdapter extends ArrayAdapter<Friend> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        try {
-            final Friend selected = getItem(position);
-            if (convertView == null) {
-                convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_friend_detail, parent, false);
-            }
-            ivProfilePic = (ImageView) convertView.findViewById(R.id.profile_picture);
-            tvName = (TextView) convertView.findViewById(R.id.player_name);
-            tvName.setText(selected.getFname());
-            if (selected.getProfile_picture_url().isEmpty()) {
-                Bundle param = new Bundle();
-                param.putBoolean("redirect", false);
-                new GraphRequest(
-                        AccessToken.getCurrentAccessToken(),
-                        "/" + selected.getId() + "/picture",
-                        param, HttpMethod.GET,
-                        new GraphRequest.Callback() {
-                            @Override
-                            public void onCompleted(GraphResponse response) {
-//                                                Log.i(this.toString(),"Profile Picture request result: " + response.toString());
-                                try {
-                                    JSONObject mData = response.getJSONObject().getJSONObject("data");
-//                                                    Log.i(this.toString(), "Data: " + mData.toString());
-                                    if (mData.has("url")) {
-                                        Log.i(this.toString(),"Profile URL: " + mData.getString("url"));
-                                        selected.setProfile_picture_url(mData.getString("url"));
-                                    }
-                                    final Bitmap mProfilePicture = new DownloadProfilePictureTask().execute(selected.getProfile_picture_url()).get();
-                                    ivProfilePic.setImageBitmap(mProfilePicture);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                } catch (ExecutionException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                ).executeAsync();
-            }
-            else {
-                final Bitmap mProfilePicture = new DownloadProfilePictureTask().execute(selected.getProfile_picture_url()).get();
-                ivProfilePic.setImageBitmap(mProfilePicture);
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        final Friend selected = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_friend_detail, parent, false);
         }
-        return convertView; // super.getView(position, convertView, parent);
+        ivProfilePic = (ImageView) convertView.findViewById(R.id.profile_picture);
+        tvName = (TextView) convertView.findViewById(R.id.player_name);
+        tvName.setText(selected.getFname());
+
+        selected.getProfile_picture(ivProfilePic);
+        return convertView;
     }
 }
